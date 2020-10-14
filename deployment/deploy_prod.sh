f727bd0feb9b97e60135bc26ded30d2f4d94976e
Prasad B <36314837+Myraa@users.noreply.github.com>
2020-10-14 17:10:24 -0500

    Update deploy_prod.sh

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ssh ubuntu@ec2-18-217-58-163.us-east-2.compute.amazonaws.com <<EOF
+ssh ubuntu@ec2-18-217-58-163.us-east-2.compute.amazonaws.com <EOF
   cd djtrump
   git pull
   source /opt/envs/djtrump/bin/activate
