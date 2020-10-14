07cc3842817cd1eaa0ca045c6a98731005bd18b4
Prasad B <36314837+Myraa@users.noreply.github.com>
2020-10-14 17:50:10 -0500

    Update deploy_prod.sh

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ssh -i ubuntu.pem ubuntu@ec2-18-217-58-163.us-east-2.compute.amazonaws.com <<EOF
+ssh -i ubuntu.pem -T ubuntu@ec2-18-217-58-163.us-east-2.compute.amazonaws.com <<EOF
   pwd
   cd /home/ubuntu/djtrump
   git clone https://github.com/Myraa/djtrump.git
