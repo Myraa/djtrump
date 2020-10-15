c5e191cfefaf9b78ffc95ebacf366c6e2c7a7ec1
Prasad B <36314837+Myraa@users.noreply.github.com>
2020-10-15 11:39:56 -0500

    Update deploy_prod.sh

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ssh -i ubuntu.pem -T ubuntu@ec2-18-217-58-163.us-east-2.compute.amazonaws.com <<EOF
+ssh -i $1 -T ubuntu@ec2-18-217-58-163.us-east-2.compute.amazonaws.com <<EOF
   pwd
   cd /home/ubuntu/djtrump
   pip install -r requirements.txt
