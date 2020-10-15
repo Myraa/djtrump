42a5ce4938bf9f5135c17cf4feeea8b98c6d8674
Prasad B <36314837+Myraa@users.noreply.github.com>
2020-10-15 11:13:24 -0500

    Update deploy_prod.sh

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -3,10 +3,6 @@
 ssh -i ubuntu.pem -T ubuntu@ec2-18-217-58-163.us-east-2.compute.amazonaws.com <<EOF
   pwd
   cd /home/ubuntu/djtrump
-  git clone https://github.com/Myraa/djtrump.git
-  source /opt/envs/djtrump/bin/activate
   pip install -r requirements.txt
-  ./manage.py migrate
-  sudo supervisorctl restart djtrump
   exit
 EOF
